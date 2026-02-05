5728d3b1f8ce0a9904d4051d62ef6752f1e52bd5
PTICYN <pticyn.leonid@mail.ru>
2026-02-05 18:10:35 +0300

    feat: introduce Game class and move game loop logic

--- main.cpp
+++ main.cpp
@@ -1,34 +1,8 @@
-#include "include/Board.h"
-using namespace sf;
-using namespace std;
-using namespace GameConfig;
+#include "include/Game.h"
 
 int main()
 {
-    RenderWindow window(VideoMode({WINDOW_WIDTH, WINDOW_HEIGHT}), "Tetris-by-Pticyn");
-
-    Board board;
-    Clock clock;
-    clock.start();
-
-    while (window.isOpen())
-    {
-        while (const optional event = window.pollEvent())
-        {
-            if (event->is<Event::Closed>())
-                window.close();
-            board.action(*event);
-        }
-
-        if (clock.getElapsedTime().asSeconds() >= SPEED_FREE_FALL)
-        {
-            board.fallCurrentTetromino();
-            clock.restart();
-        }
-
-        window.clear(Color::Black);
-        board.draw(window);
-        window.display();
-    }
+    Game game;
+    game.run();
     return 0;
 }
